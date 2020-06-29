2ac6d9a727e9a6f6c62fffcae03efc73ad8b143d
Benedikt Gollatz <ben@differentialschokolade.org>
2020-06-29 15:11:31 +0200

    Add simple confirmation e-mail resending functionality in admin

--- ams/modules/petition/admin.py
+++ ams/modules/petition/admin.py
@@ -1,7 +1,16 @@
 from django.contrib import admin
+from django.http import HttpResponse
 from .models import Signature
 
 class SignatureAdmin(admin.ModelAdmin):
-    pass
+    actions = ['resend_email']
+
+    def resend_email(self, request, queryset):
+        for signature in queryset:
+            if not signature.confirmed:
+                signature.send_confirmation_email(request)
+        return HttpResponse('OK')
+
+    resend_email.short_description = "Bestätigungsmail erneut senden"
 
 admin.site.register(Signature, SignatureAdmin)
